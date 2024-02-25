187789ec11cd6d54193947c71098ea0b6ac09529
DeclanTurnbaugh <declantcollege@gmail.com>
2024-02-25 13:30:48 -0700

    Added header boilerplate

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <header>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
