80eab278a3d0418b2cd1975f6ba442cf99f4e211
DeclanTurnbaugh <declantcollege@gmail.com>
2024-02-25 13:24:38 -0700

    Added filler content to home page as well as rudimentary css stylings

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import "./Home.css";
 
 const Home = () => {
-  return <>Home</>;
+  return (
+    <div className="home-container">
+      <div className="home-hero">
+        <h1>{`Hi, I'm Declan`}</h1>
+        <p>I love creating beautiful user experiences</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
